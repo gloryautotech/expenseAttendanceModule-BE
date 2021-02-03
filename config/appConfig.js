eb02eb61d9fabd41a044a0f5396779b903432bbc
yash balkhande <yash.gloryautotech@gmail.com>
2021-02-03 15:51:20 +0530

    Make it better

--- config/appConfig.js
+++ config/appConfig.js
@@ -5,10 +5,8 @@ appConfig.port = process.env.PORT || 4848,
 appConfig.allowedCorsOrigin = "*";
 appConfig.env = "dev";
 appConfig.db = {
-    // uri: 'mongodb://localhost:27017/blogAppDB',
-    //uri:"mongodb+srv://kishan:yadav@cluster0.oluxo.mongodb.net/Demo?retryWrites=true&w=majority"
-    // uri: 'mongodb://localhost:27017/DemoTest',
-    uri:'mongodb+srv://databse:database@cluster0.r3dbh.mongodb.net/Emp?retryWrites=true&w=majority'
+    //uri:'mongodb+srv://databse:database@cluster0.r3dbh.mongodb.net/Emp?retryWrites=true&w=majority'
+    uri:'mongodb+srv://databse:database@cluster0.u1a2b.mongodb.net/Expenses?retryWrites=true&w=majority'
 }
 appConfig.apiVersion = '/api/v1';
 
--- models/attendanceModel.js
+++ models/attendanceModel.js
@@ -6,7 +6,7 @@ let attendanceSchema = new Schema({
 
     attendanceId:{
         type:String,
-        default:''
+        unique: true
     },
     employeeName:{
         type:String,
